61b5dc773ff5339c7dc7e5b6b275468a87a1cfbd
Oscar Guerra <desarrolloti32_lem@castores.com.mx>
2021-12-15 17:56:38 -0600

    Test diferentes archivos

--- src/mx/com/gm/test/VentasTest.java
+++ src/mx/com/gm/test/VentasTest.java
@@ -5,14 +5,14 @@ import mx.com.gm.ventas.*;
 public class VentasTest {
     public static void main(String[] args){
         Producto producto1  = new Producto("Caguamas", 36.50);
-        Producto producto2  = new Producto("Pantalon", 299.00);
-        Producto producto3  = new Producto("Camisa", 199.00);
+//        Producto producto2  = new Producto("Pantalon", 299.00);
+//        Producto producto3  = new Producto("Camisa", 199.00);
         
         Orden orden1 = new Orden();
-        orden1.agregarProduco(producto1);
-        orden1.agregarProduco(producto2);
-        orden1.agregarProduco(producto3);
-        orden1.mostrarOrden();
+//        orden1.agregarProduco(producto1);
+//        orden1.agregarProduco(producto2);
+//        orden1.agregarProduco(producto3);
+//        orden1.mostrarOrden();
 
     }
 }
